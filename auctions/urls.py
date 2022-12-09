f3a7cfdec98d347fb0860272e5abaec77c0b0d65
GF <farnellg@gmail.com>
2022-12-09 13:37:28 -0500

    change

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("active", views.activeListings, name="activeListings"),
+    # path("", views.index, name="index"),
+    path("", views.activeListings, name="activeListings"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
--- auctions/views.py
+++ auctions/views.py
@@ -34,7 +34,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("activeListings"))
         else:
             return render(request, "auctions/login.html", {
                 "message": "Invalid username and/or password."
@@ -45,7 +45,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("activeListings"))
 
 
 def register(request):
@@ -70,6 +70,6 @@ def register(request):
                 "message": "Username already taken."
             })
         login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+        return HttpResponseRedirect(reverse("activeListings"))
     else:
         return render(request, "auctions/register.html")
