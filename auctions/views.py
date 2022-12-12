6f76d5e26802a2f5b2e00a0f949764cdd378bd7c
GF <farnellg@gmail.com>
2022-12-12 14:00:12 -0500

    commit

--- auctions/views.py
+++ auctions/views.py
@@ -24,38 +24,92 @@ def listings(request, listing_id):
     listing = Listings.objects.get(id=listing_id)
     return render(request, "auctions/listing.html", {"listing": listing})
 
-#TODO: This needs work.  users where watchlist = true.  Change watchlist to Boolean.  Need to look at the examples.
+# TODO: This needs work.  users where watchlist = true.  Change watchlist to Boolean.  Need to look at the examples.
+
+
 def watchlist(request, listing_id):
+
     print(listing_id)
     test = Listings.objects.get(id=listing_id)
+    # This prints the listing.
     print(test)
     test2 = test.watchers.all()
-    #This prints the watcher for the listing.
+    # This prints the watchers for the listing id.
     print(test2)
-    #This gets the current username.
-    username = request.user.username
-    print(username)
 
-    listings = Listings.objects.all()
-
-    for x in listings:
-        print(x)
+    #Turn the queryset into a list.
+    userList = list(test2)
 
 
 
+
+
+    user = request.user.username
+
+    # This loops through the watcher queryset.
+    for x in userList:
+
+
+        if x.username == user:
+            # print("This item is already on the user's watchlist!")
+            return HttpResponseRedirect(reverse("activeListings"))
+            #Need to exit at this point and not do anything.
+
+
+   #Add the item to the watchlist.
+
+    return HttpResponse("Need to add item to watchlist!")
+
+
+
+
+
+
+
+
+
+
+    # TODO:
+    # Need to search through queryset tests for 'gfarnell'
+    # test4 = Listings.objects.filter(user=user)
+    # print(test4)
+
+    # # This gets the current username.
+    # username2 = request.user.username
+    # print(username2)
+
+    # listings = Listings.objects.all().values()
     # print(listings)
 
+    # usernameWatchlist = Listings.objects.values('watchlist')
+    # print(usernameWatchlist)
 
+    # # Need the id here.
+    # usernameWatchlist = Listings.objects.filter(watchers=1)
+    # print(usernameWatchlist)
 
+    # getUserName = User.objects.filter(username=request.user.username)
+    # if getUserName:
+    #     print("That username")
 
+    # for x in listings:
+    #     print(x)
+    #     print(test.watchers.all().values())
+    #     # This prints all the values of the User object
+    #     print(User.objects.all().values())
 
+    # print(listings)
 
     # print(test2)
     # test = Listings.objects.filter()
     # test = Listings.objects.get(User.username)
     # print(test)
 
-    return HttpResponse("Called the watchlist method!")
+
+
+
+def new_func(listings):
+    listings.id
 
 
 def login_view(request):
