f8f2260aa68ec123cfdfab400cc59bb8e736891b
GF <farnellg@gmail.com>
2022-12-26 18:49:52 -0500

    change

--- commerce/settings.py
+++ commerce/settings.py
@@ -123,7 +123,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
