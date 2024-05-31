75ae3fddfa26f956dd7f54fda238cdffdb437936
GF <farnellg@gmail.com>
2024-05-31 16:21:42 -0400

    latest updates.

--- commerce/settings.py
+++ commerce/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),  # Use /tmp for write access'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
